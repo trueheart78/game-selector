725a28be04ee2cade10279ae93faf28c308de2f5
Josh Mills <jmills@covermymeds.com>
2017-07-06 07:36:01 -0400

    API endpoints added.

--- lib/server.rb
+++ lib/server.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'json'
 require_relative 'selector'
 
 get '/' do
@@ -11,6 +12,20 @@ end
 
 get '/types' do
   Selector::TYPES.join ', '
+end
+
+before '/api*' do
+  content_type 'application/json'
+end
+
+get '/api' do
+  { game: Selector.new(:unplayed, params[:site]).random.to_s }.to_json
+end
+
+get '/api/:type' do
+  return 404 unless Selector::TYPES.include? params[:type]
+  Selector.new(params[:type]).random.to_s
+  { game: Selector.new(params[:type]).random.to_s }.to_json
 end
 
 get '/:type' do
